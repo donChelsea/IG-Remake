899833b60a3cc69fc85f5f452d5178a8bd915059
Chelsea Katsidzira <chelsea.katsidzira@bounteous.com>
2021-07-31 16:39:36 -0400

    created login ui

--- app/src/main/java/com/example/ig_remake/MainActivity.kt
+++ app/src/main/java/com/example/ig_remake/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.ig_remake
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.ig_remake.databinding.ActivityMainBinding
+import com.example.ig_remake.databinding.FragmentHomeBinding
+import com.example.ig_remake.presentation.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val homeFragment = HomeFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(homeFragment)
+        controlBottomNav()
+    }
+
+    private fun controlBottomNav() {
+        binding.bottomNavigatinView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+//                R.id.exploreFragment -> replaceFragment(exploreFragment)
+                R.id.homeFragment -> replaceFragment(homeFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        if (fragment != null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
     }
 }
---
+++ app/src/main/java/com/example/ig_remake/presentation/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.example.ig_remake.presentation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ig_remake.MainActivity
+import com.example.ig_remake.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
